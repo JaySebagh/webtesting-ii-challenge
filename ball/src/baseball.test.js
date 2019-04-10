9189c013563e76c290c22ba6499466e48ecc44fa
JaySebagh <jaysebagh@gmail.com>
2019-04-09 19:57:43 -0700

    added some testing. Was not ablt to do everything I wanted. 8PM here, still need to eat and get ready for tomorrow. Bed time in 1h.

--- ball/src/baseball.test.js
+++ ball/src/baseball.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 // import ReactDom from 'react-dom';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 import App from './App.js'
+import Display from './components/Display.js'
+import Dashboard from './components/Dashboard.js'
 
 it('renders successfully', () => {
     render(<App />)
+    render(<Display />)
+    render(<Dashboard />)
 });
